16bab79d63e8e0c96b3826db50c7138fbc2700f5
Alireza Karimi <Km0fficial@yahoo.com>
2025-05-02 18:42:40 +0330

    create component sidebar

---
+++ src/components/sidebar/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import './sidebar.css';
+
+const Sidebar: FC = () => {
+  return <div className={'wrapper-sidebar'}>Folder Path Here!</div>;
+};
+
+export default Sidebar;
